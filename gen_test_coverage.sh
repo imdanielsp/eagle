3751e4b29a73fed12d4da6d0b23b7c7e7b5b99d4
Daniel Santos <dsantosp12@gmail.com>
2020-12-10 16:34:46 -0500

    Fixes file rename

--- gen_test_coverage.sh
+++ gen_test_coverage.sh
@@ -6,5 +6,5 @@ meson compile -C builddir
 $TEST_EXEC
 llvm-profdata merge -sparse default.profraw -o default.profdata
 llvm-cov report $TEST_EXEC -instr-profile=default.profdata
-llvm-cov export $TEST_EXEC -instr-profile=default.profdata -format=lcov include src > default.lcov
+llvm-cov export $TEST_EXEC -instr-profile=default.profdata -format=lcov include src > default.info
 genhtml -t "Eagle Test Coverage" default.info --output-directory test_coverage_result
