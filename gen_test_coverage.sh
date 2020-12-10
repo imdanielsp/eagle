0e660edbb30c8a2f2eb029b9fec61d13ae1ae666
Daniel Santos <dsantosp12@gmail.com>
2020-12-10 16:28:33 -0500

    Add test coverage infrastructure

---
+++ gen_test_coverage.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TEST_EXEC=./builddir/eagle_test
+
+meson compile -C builddir
+$TEST_EXEC
+llvm-profdata merge -sparse default.profraw -o default.profdata
+llvm-cov report $TEST_EXEC -instr-profile=default.profdata
+llvm-cov export $TEST_EXEC -instr-profile=default.profdata -format=lcov include src > default.lcov
+genhtml -t "Eagle Test Coverage" default.info --output-directory test_coverage_result
