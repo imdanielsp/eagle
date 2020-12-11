fc79db765fef58cbebb1a6dbe40c29302aeaea5f
Daniel Santos <dsantosp12@gmail.com>
2020-12-11 11:44:23 -0500

    Add more test for handler registry

--- tests/handler_registry_test.cc
+++ tests/handler_registry_test.cc
@@ -12,6 +12,57 @@ TEST(HandlerRegistryTest, RegisterFunctionHandler) {
   EXPECT_TRUE(result);
 }
 
+TEST(HandlerRegistryTest, RegisterFunctionHandlersForSameURI) {
+  eagle::handler_registry<eagle::handler_fn_type> registry;
+  {
+    auto result = registry.register_handler(
+        http::verb::get, "/some-endpoint",
+        [](const auto&, auto&) -> bool { return true; });
+    EXPECT_TRUE(result);
+
+    auto [opt_handler, status] =
+        registry.get_handler_for(http::verb::get, "/some-endpoint");
+    EXPECT_TRUE(status);
+    EXPECT_TRUE(opt_handler.has_value());
+  }
+
+  {
+    auto result = registry.register_handler(
+        http::verb::post, "/some-endpoint",
+        [](const auto&, auto&) -> bool { return true; });
+    EXPECT_TRUE(result);
+
+    auto [opt_handler, status] =
+        registry.get_handler_for(http::verb::post, "/some-endpoint");
+    EXPECT_TRUE(status);
+    EXPECT_TRUE(opt_handler.has_value());
+  }
+
+  {
+    auto result = registry.register_handler(
+        http::verb::put, "/some-endpoint",
+        [](const auto&, auto&) -> bool { return true; });
+    EXPECT_TRUE(result);
+
+    auto [opt_handler, status] =
+        registry.get_handler_for(http::verb::put, "/some-endpoint");
+    EXPECT_TRUE(status);
+    EXPECT_TRUE(opt_handler.has_value());
+  }
+
+  {
+    auto result = registry.register_handler(
+        http::verb::delete_, "/some-endpoint",
+        [](const auto&, auto&) -> bool { return true; });
+    EXPECT_TRUE(result);
+
+    auto [opt_handler, status] =
+        registry.get_handler_for(http::verb::delete_, "/some-endpoint");
+    EXPECT_TRUE(status);
+    EXPECT_TRUE(opt_handler.has_value());
+  }
+}
+
 TEST(HandlerRegistryTest, RegisterFunctionHandlerTwiceForSameEndpoint) {
   eagle::handler_registry<eagle::handler_fn_type> registry;
 
@@ -48,11 +99,26 @@ TEST(HandlerRegistryTest, GetRegisteredHandler) {
   }
 }
 
-TEST(HandlerRegistryTest, GetUnRegisteredHandler) {
+TEST(HandlerRegistryTest, GetUnRegisteredFunctionHandler) {
   eagle::handler_registry<eagle::handler_fn_type> registry;
   auto [opt_handler, status] =
       registry.get_handler_for(http::verb::get, "/some-endpoint");
   EXPECT_FALSE(status);
+  EXPECT_FALSE(opt_handler.has_value());
+}
+
+TEST(HandlerRegistryTest, GetUnsupportedMethodForFunctionHandler) {
+  eagle::handler_registry<eagle::handler_fn_type> registry;
+  {
+    auto result = registry.register_handler(
+        http::verb::get, "/some-endpoint",
+        [](const auto&, auto&) -> bool { return true; });
+    EXPECT_TRUE(result);
+  }
+
+  auto [opt_handler, result] =
+      registry.get_handler_for(http::verb::head, "/some-endpoint");
+  EXPECT_FALSE(result);
   EXPECT_FALSE(opt_handler.has_value());
 }
 
@@ -67,6 +133,30 @@ TEST(HandlerRegistryTest, RegisterObjectHandler) {
       registry.get_handler_for(eagle::all_method, "/some-endpoint");
   EXPECT_TRUE(status);
   EXPECT_TRUE(opt_handler.has_value());
+}
+
+TEST(HandlerRegistryTest, RegisterObjectHandlerTwiceForSameEndpoint) {
+  eagle::handler_registry<eagle::handler_type> registry;
+  HandlerMock mock_handler;
+
+  auto result = registry.register_handler("/some-endpoint", mock_handler);
+  EXPECT_TRUE(result);
+
+  result = registry.register_handler("/some-endpoint", mock_handler);
+  EXPECT_FALSE(result);
+
+  auto [opt_handler, status] =
+      registry.get_handler_for(eagle::all_method, "/some-endpoint");
+  EXPECT_TRUE(status);
+  EXPECT_TRUE(opt_handler.has_value());
+}
+
+TEST(HandlerRegistryTest, GetUnRegisteredObjectHandler) {
+  eagle::handler_registry<eagle::handler_type> registry;
+  auto [opt_handler, status] =
+      registry.get_handler_for(http::verb::get, "/some-endpoint");
+  EXPECT_FALSE(status);
+  EXPECT_FALSE(opt_handler.has_value());
 }
 
 TEST(HandlerRegistryTest, HasFunctionHandler) {
